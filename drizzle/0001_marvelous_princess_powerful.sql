70534015acc5ad3257b9ca932601f8b48817a68b
Levent Deniz <leventdeniz@gmx.de>
2025-01-10 22:36:45 +0100

    attempt to run migration again

--- drizzle/0001_marvelous_princess_powerful.sql
+++ drizzle/0001_marvelous_princess_powerful.sql
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS `drinks` (
 	CONSTRAINT `drinks_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
--- ALTER TABLE `ratings` DROP FOREIGN KEY `ratings_beer_beers_id_fk`;
--- DROP TABLE `beers`;--> statement-breakpoint
+ALTER TABLE `ratings` DROP FOREIGN KEY `ratings_beer_beers_id_fk`;
+DROP TABLE `beers`;--> statement-breakpoint
 --> statement-breakpoint
--- ALTER TABLE `drinking_logs` ADD CONSTRAINT `drinking_logs_drink_drinks_id_fk` FOREIGN KEY (`drink`) REFERENCES `drinks`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE `drinking_logs` ADD CONSTRAINT `drinking_logs_rating_ratings_id_fk` FOREIGN KEY (`rating`) REFERENCES `ratings`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE `drinks` ADD CONSTRAINT `drinks_brewery_breweries_id_fk` FOREIGN KEY (`brewery`) REFERENCES `breweries`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE `ratings` ADD CONSTRAINT `ratings_beer_drinks_id_fk` FOREIGN KEY (`beer`) REFERENCES `drinks`(`id`) ON DELETE no action ON UPDATE no action;
+ALTER TABLE `drinking_logs` ADD CONSTRAINT `drinking_logs_drink_drinks_id_fk` FOREIGN KEY (`drink`) REFERENCES `drinks`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `drinking_logs` ADD CONSTRAINT `drinking_logs_rating_ratings_id_fk` FOREIGN KEY (`rating`) REFERENCES `ratings`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `drinks` ADD CONSTRAINT `drinks_brewery_breweries_id_fk` FOREIGN KEY (`brewery`) REFERENCES `breweries`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `ratings` ADD CONSTRAINT `ratings_beer_drinks_id_fk` FOREIGN KEY (`beer`) REFERENCES `drinks`(`id`) ON DELETE no action ON UPDATE no action;
