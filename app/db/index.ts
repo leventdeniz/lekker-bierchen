c9f675fe627eae60b1a0887a30dd7bf621efbf17
Levent Deniz <leventdeniz@gmx.de>
2025-01-16 15:00:05 +0100

    try pool instead of single connection

--- app/db/index.ts
+++ app/db/index.ts
@@ -3,9 +3,9 @@ import { drizzle } from "drizzle-orm/mysql2";
 import * as schema from './schema';
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-                                                  uri: process.env.DATABASE_URL,
-                                                });
+const connection = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+});
 const db = drizzle({ client: connection, schema, mode: "default"});
 
 export { db };
